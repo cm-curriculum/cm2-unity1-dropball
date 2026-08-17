cead923661fe0e60a9451094accb9cf3de1c304b
VictorTPhan <71913548+VictorTPhan@users.noreply.github.com>
2026-08-17 13:34:17 -0700

    Revert "Update Unity packages; add MultiplayerManager"
    This reverts commit c1a9e7f674f03ff560c8b1b11a861efa79371c01.

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -38,19 +38,19 @@ public class BallControl : MonoBehaviour
             if (force == 0 || velocity.magnitude == 0)
                 return;
 
-            velocity = velocity + velocity.normalized * 0.2f * rigidbody.linearDamping;
+            velocity = velocity + velocity.normalized * 0.2f * rigidbody.drag;
 
             //force = 1 => need 1 s to reach velocity (if mass is 1) => force can be max 1 / Time.fixedDeltaTime
             force = Mathf.Clamp(force, -rigidbody.mass / Time.fixedDeltaTime, rigidbody.mass / Time.fixedDeltaTime);
 
             //dot product is a projection from rhs to lhs with a length of result / lhs.magnitude https://www.youtube.com/watch?v=h0NJK4mEIJU
-            if (rigidbody.linearVelocity.magnitude == 0)
+            if (rigidbody.velocity.magnitude == 0)
             {
                 rigidbody.AddForce(velocity * force, mode);
             }
             else
             {
-                var velocityProjectedToTarget = (velocity.normalized * Vector3.Dot(velocity, rigidbody.linearVelocity) / velocity.magnitude);
+                var velocityProjectedToTarget = (velocity.normalized * Vector3.Dot(velocity, rigidbody.velocity) / velocity.magnitude);
                 rigidbody.AddForce((velocity - velocityProjectedToTarget) * force, mode);
             }
     }
